c06b208206bc00ed5c416457eabec1ad2a3a1d3c
jhz-code <zhuok520@qq.com>
2021-10-08 01:19:21 +0800

    自定义文件名

--- src/core/TopUpload.php
+++ src/core/TopUpload.php
@@ -25,7 +25,8 @@ class TopUpload
                 mkdir($LocalPath,0777,true);
             }
             //移动文件到目标路径
-            $saveFileName = $fileName??$result['fileName'];
+            $saveFileName = $fileName?$fileName.substr(strrchr($result['fileName'], '.'), 1)?$result['fileName'];
+
             if(copy($result['filePath'],$LocalPath.'/'.$saveFileName)){
                 if(!$is_store){
                     $upload->deleteFile();//删除缓存库
